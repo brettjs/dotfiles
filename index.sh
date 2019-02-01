fdd074b409525cb82fc44ed8bc8dec53efb05896
Brett Slone <brett@nomadhealth.com>
2019-02-01 17:22:26 -0500

    chore(dotfiles): Initial commit

---
+++ index.sh
@@ -0,0 +1,5 @@
+dotfiles_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+source ${dotfiles_dir}/src/path.sh
+source ${dotfiles_dir}/src/git-completion.sh
+source ${dotfiles_dir}/src/ps1.sh
---
+++ src/path.sh
@@ -0,0 +1,6 @@
+export PATH="/usr/local/opt/icu4c/bin:$PATH"
+export PATH="/usr/local/opt/icu4c/sbin:$PATH"
+export PATH="/usr/local/opt/mongodb@3.6/bin:$PATH"
+export PATH="/usr/local/opt/mongodb@3.4/bin:$PATH"
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
